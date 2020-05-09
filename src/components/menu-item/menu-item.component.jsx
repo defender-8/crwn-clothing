ad872d078ca62eccf5e768da6a8f6df554f55083
Alex Kondratenko <kondratenko.ov.8@gmail.com>
2020-05-09 18:29:51 +0300

    - Directory, menu-item components - eslint

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 import './menu-item.style.scss';
 
 function MenuItem({ title, imageUrl, size }) {
-    return (
-        <div className={`menu-item ${size}`}>
-            <div
-                style={{
-                    backgroundImage: `url(${imageUrl})`,
-                }}
-                className="background-image"
-            />
-            <div className="content">
-                <h1 className="title">{title}</h1>
-                <div className="subtitle">Shop now</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={`menu-item ${size}`}>
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+        className="background-image"
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <div className="subtitle">Shop now</div>
+      </div>
+    </div>
+  );
 }
 
 export default MenuItem;

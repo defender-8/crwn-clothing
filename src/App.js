37ee608608ab44b9b9e4247749ef229fed99dc04
Alex Kondratenko <kondratenko.ov.8@gmail.com>
2020-05-27 00:06:27 +0300

    Unused Link is removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 const HatsPage = () => (

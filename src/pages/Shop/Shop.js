264e4c4bfefdcf0c67f8e84035ff4b521c55633a
Alex Kondratenko <kondratenko.ov.8@gmail.com>
2020-09-14 13:53:50 +0300

    - CollectionPage, shop data normalization (array to object)

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,28 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './data';
+import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
+import CollectionPage from '../CollectionPage/CollectionPage';
 
-import CollectionPreview from '../../components/CollectionPreview/CollectionPreview';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
+function ShopPage({ match }) {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
 }
 
 export default ShopPage;
